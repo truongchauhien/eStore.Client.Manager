113bfceccd1bda7f3c04b9da41736a9e3230a3c9
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-07 19:43:15 +0700

    Change root reducer.

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './commons/user/user-reducer';
 import productReducer from './modules/manager/product/product-reducer';
 import categoryReducer from './modules/manager/category/category-reducer';
+import distributorReducer from './modules/manager/distributor/distributor-reducer';
 
 const rootReducer = combineReducers({
     user: userReducer,
     products: productReducer,
-    categories: categoryReducer
+    categories: categoryReducer,
+    distributors: distributorReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
