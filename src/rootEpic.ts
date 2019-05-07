56adca80dd2af25ae39beed783c63b76d7e87ec3
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-07 15:37:53 +0700

    Change root epic.

--- src/rootEpic.ts
+++ src/rootEpic.ts
@@ -1,4 +1,18 @@
 import { combineEpics } from 'redux-observable';
 import { userLoginRequestEpic } from './commons/user/user-epics';
+import { productListEpic } from './modules/manager/product/product-epics';
+import {
+    listCategoryEpic,
+    createCategoryEpic,
+    updateCategoryEpic,
+    deleteCategoryEpic
+} from './modules/manager/category/category-epics';
 
-export default combineEpics(userLoginRequestEpic);
+export default combineEpics(
+    userLoginRequestEpic,
+    productListEpic,
+    listCategoryEpic,
+    createCategoryEpic,
+    updateCategoryEpic,
+    deleteCategoryEpic
+);
