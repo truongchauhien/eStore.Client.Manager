acf284bf42b27c3f5640ddef8e4c9de991aa04ed
Truong Chau Hien <truongchauhien@gmail.com>
2019-05-31 09:31:15 +0700

    Update main layout.

--- src/modules/main-layout/main-layout-container.tsx
+++ src/modules/main-layout/main-layout-container.tsx
@@ -5,9 +5,16 @@ import { Sidebar, Segment, Menu, Icon, Header, Image, Button, Dropdown, MenuItem
 import * as styles from './styles.scss';
 import { AppState } from '../../rootReducer';
 import { userLogoutRequest } from '../../commons/user/user-actions';
-import ProductManagement from '../manager/product/product-management';
-import DistributorManagement from '../manager/distributor/distributor-management';
-import CategoryManagement from '../manager/category/category-management';
+import ProductManagement from '../product/product-management';
+import DistributorManagement from '../distributor/distributor-management';
+import CategoryManagement from '../category/category-management';
+import Order from '../order/order';
+import WorkerManagement from '../employee/employee-management';
+import WorkShiftManagement from '../work-shift/work-shift-management';
+import Supply from '../supply/supply';
+import ModalRoot from '../commons/modal/modal-root';
+import Report from '../report/report';
+import Can from '../../commons/permission/can-component';
 
 interface IMainLayoutProps {
     userName: string,
@@ -26,7 +33,7 @@ class MainLayout extends React.Component<IMainLayoutProps, IMainLayoutState> {
         this.state = {
             sideBarVisible: false,
             menuActiveItem: 'home',
-            selectedBusiness: 'category'
+            selectedBusiness: ''
         }
     }
 
@@ -84,54 +91,85 @@ class MainLayout extends React.Component<IMainLayoutProps, IMainLayoutState> {
                             visible={sideBarVisible}
                             width='thin'
                         >
-                            <Menu.Item as='a' onClick={() => this.handleBusinessClick('')}>
-                                <Icon name='shopping cart' />
-                                Tạo hóa đơn
+                            <Can requiredResource='order' requiredPermission='create'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('order')}>
+                                    <Icon name='shopping cart' />
+                                    Tạo hóa đơn
+                                </Menu.Item>
+                            </Can>
+                            <Can requiredResource='product' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('product')}>
+                                    <Icon name='cubes' />
+                                    Sản phẩm
+                                </Menu.Item>
+                            </Can>
+                            <Can requiredResource='category' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('category')}>
+                                    <Icon name='folder' />
+                                    Danh mục
+                                </Menu.Item>
+                            </Can>
+                            <Can requiredResource='distributor' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('distributor')}>
+                                    <Icon name='industry' />
+                                    Nhà phân phối
+                                </Menu.Item>
+                            </Can>
+                            <Can requiredResource='supply' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('supply')}>
+                                    <Icon name='truck' />
+                                    Nhập hàng
                             </Menu.Item>
-                            <Menu.Item as='a'>
-                                <Icon name='user secret' />
-                                Nhân viên
-                            </Menu.Item>
-                            <Menu.Item as='a' onClick={() => this.handleBusinessClick('product')}>
-                                <Icon name='cubes' />
-                                Sản phẩm
-                            </Menu.Item>
-                            <Menu.Item as='a' onClick={() => this.handleBusinessClick('category')}>
-                                <Icon name='folder' />
-                                Danh mục
-                            </Menu.Item>
-                            <Menu.Item as='a' onClick={() => this.handleBusinessClick('distributor')}>
-                                <Icon name='industry' />
-                                Nhà phân phối
-                            </Menu.Item>
-                            <Menu.Item as='a'>
-                                <Icon name='truck' />
-                                Nhập hàng
-                            </Menu.Item>
-                            <Menu.Item as='a' onClick={() => this.handleBusinessClick('report')}>
-                                <Icon name='chart bar' />
-                                Báo cáo
-                            </Menu.Item>
+                            </Can>
+                            <Can requiredResource='report' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('report')}>
+                                    <Icon name='chart bar' />
+                                    Báo cáo
+                                </Menu.Item>
+                            </Can>
+                            <Can requiredResource='employee' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('employee')}>
+                                    <Icon name='user secret' />
+                                    Nhân viên
+                                </Menu.Item>
+                            </Can>
+                            <Can requiredResource='workShift' requiredPermission='read'>
+                                <Menu.Item as='a' onClick={() => this.handleBusinessClick('workShift')}>
+                                    <Icon name='time' />
+                                    Phân ca
+                                </Menu.Item>
+                            </Can>
                         </Sidebar>
 
                         <Sidebar.Pusher dimmed={sideBarVisible}>
                             {(() => {
                                 switch (selectedBusiness) {
+                                    case 'order':
+                                        return <Order />
                                     case 'product':
-                                        return <ProductManagement />;
+                                        return (<ProductManagement />);
                                     case 'distributor':
                                         return <DistributorManagement />;
                                     case 'category':
                                         return <CategoryManagement />;
-
+                                    case 'supply':
+                                        return <Supply />;
+                                    case 'report':
+                                        return <Report />
+                                    case 'employee':
+                                        return <WorkerManagement />;
+                                    case 'workShift':
+                                        return <WorkShiftManagement />;
                                     default:
                                         return null;
                                 }
                             })()}
                         </Sidebar.Pusher>
                     </Sidebar.Pushable>
+
+                    <ModalRoot />
                 </div>
-            </div>
+            </div >
         )
     }
 }
